3820af09202a139acd9d66f503e576f9ca37de1c
StingerBlade <arras2805200@gmail.com>
2024-10-02 21:57:34 -0600

    tarjetas ya mejoradas con imagenes

--- reactprueba/src/components/Contenido.js
+++ reactprueba/src/components/Contenido.js
@@ -4,20 +4,42 @@ function Contenido() {
     <div className="content">
       <div className="divM">
         <div className="divN">
-          
-            <p>Tarjeta 1</p>
-          
+           <div className="image">
+            <img 
+            src="/hacked.jpg"
+            className="img"
+            />
+           </div>
+           <div className="textimage"></div>  
         </div>
         <div className="divN">
-          <p>Tarjeta 1</p>
+        <div className="image">
+        <img 
+            src="/shodan-web.png"
+            className="img"
+            />
+        </div>
+        <div className="textimage"></div>  
         </div>
       </div>
       <div className="divM">
         <div className="divN">
-          <p>Tarjeta 1</p>
+        <div className="image">
+        <img 
+            src="/nmap.jpg"
+            className="img"
+            />
+        </div>
+        <div className="textimage"></div>  
         </div>
         <div className="divN">
-          <p>Tarjeta 1</p>
+        <div className="image">
+        <img 
+            src="/images.png"
+            className="img"
+            />  
+        </div>
+        <div className="textimage"></div>  
         </div>
       </div>
     </div>
